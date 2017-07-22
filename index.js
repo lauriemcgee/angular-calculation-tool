d7dc7007c60124faa3e2badc56de204f27344626
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-07-22 17:35:54 -0500

    lumen options working-tutorial outdated, using later angular version

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hey');
+const angular = require('angular');
+var myApp = angular.module('myApp', []);
+
+myApp.controller('calculatorController', function($scope) {
+  var vm = $scope;
+  vm.lumen_options = [375, 600, 900, 1125, 1600];
+  vm.current_lumens = 600;
+
+});
