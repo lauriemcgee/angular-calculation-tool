be24c0b7d956b30045de15434a964dd5f968ceb6
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-07-22 19:23:35 -0500

    set conversion rates, ensured the function for changing html based on rate works

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ myApp.controller('calculatorController', function($scope) {
   var vm = $scope;
   vm.lumen_options = [375, 600, 900, 1125, 1600];
   vm.current_lumens = 600;
+  vm.current_cost = 12;
+  vm.current_hours = 3;
+  vm.total_days = 365;
+
+
+  vm.inc_conversion = .0625;
+  vm.hal_conversion = .0450;
+  vm.cfl_conversion = .0146;
+  vm.led_conversion = .0125;
+
+  vm.calculate = function() {
+    
+    
+  };
+
+  vm.calculate();
 
 });
